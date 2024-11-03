dc3ed49dbb5cae87864fde1ba8c10bc95a494f5d
ToHeCo <tornoema@live.dk>
2024-11-03 11:57:05 +0100

    Added movement to player, refactored the gameplay loop

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
 # Packages
 import pygame
 from constants import *
-
+from player import Player
 #Main 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
+
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color = (0,0,0))
+        
+
+
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        dt = clock.tick(60)/1000
+
+        #Framerate
+        dt = clock.tick(60)/1000    
+
         
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
